865e465219713d09abfd3cf96e63932feb6af9d7
an03er <144229434+an03er@users.noreply.github.com>
2025-11-08 15:03:43 +0300

    Update 4.sh

--- bash/4.sh
+++ bash/4.sh
@@ -4,10 +4,10 @@ planet="$1"
 case "$planet" in
 	"Mercury"|"Venus") echo "0";;
 	"Mars") echo "2";;
-	"Jupiter" echo "98";;
-	"Saturn" echo "83";;
-	"Uranus" echo "27";;
-	"Neptune" echo "14";;
-	"Earth" echo "1";;
+	"Jupiter") echo "98";;
+	"Saturn") echo "83";;
+	"Uranus") echo "27";;
+	"Neptune") echo "14";;
+	"Earth") echo "1";;
 	*) echo "Unknown planet";;
 esac
