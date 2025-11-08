6c1dc592a2dfca51c1a91a56e86dffb3bad7f40f
an03er <144229434+an03er@users.noreply.github.com>
2025-11-08 15:03:07 +0300

    Update 4.sh

--- bash/4.sh
+++ bash/4.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 planet="$1"
 
-if [ "$planet" = "Mercury" ]; then
-	echo "0"
-	exit
-fi
-if [ "$planet" = "Venus" ]; then
-	echo "0"
-	exit
-fi
-if [ "$planet" = "Mars" ]; then
-	echo "2"
-	exit
-fi
-if [ "$planet" = "Jupiter" ]; then
-	echo "98"
-	exit
-fi
-if [ "$planet" = "Saturn" ]; then
-	echo "83"
-	exit
-fi
-if [ "$planet" = "Uranus" ]; then
-	echo "27"
-	exit
-fi
-if [ "$planet" = "Neptune" ]; then
-	echo "14"
-	exit
-fi
-if [ "$planet" = "Earth" ]; then
-	echo "1"
-	exit
-fi
-
-echo "Unknown planet"
+case "$planet" in
+	"Mercury"|"Venus") echo "0";;
+	"Mars") echo "2";;
+	"Jupiter" echo "98";;
+	"Saturn" echo "83";;
+	"Uranus" echo "27";;
+	"Neptune" echo "14";;
+	"Earth" echo "1";;
+	*) echo "Unknown planet";;
+esac
